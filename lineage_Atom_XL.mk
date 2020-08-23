0f81f12fc062ea28c6ccb469ff8250ab70a23742
Matthias Leitl <a.dead.trousers@gmail.com>
2020-08-23 16:49:53 +0200

    Case-error

--- lineage_Atom_XL.mk
+++ lineage_Atom_XL.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from Atom_XL device
-$(call inherit-product, device/unihertz/Atom_XL/device.mk)
+$(call inherit-product, device/Unihertz/Atom_XL/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
