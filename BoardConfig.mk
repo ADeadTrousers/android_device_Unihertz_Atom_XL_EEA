ae966eb6dfed6c9eb20b730c16e0870883e04411
Matthias Leitl <a.dead.trousers@gmail.com>
2021-03-26 16:48:50 +0100

    Also move private sepolicy to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,5 +7,8 @@ DEVICE_PATH := device/Unihertz/Atom_XL
 
 -include device/Unihertz/Atom_LXL/BoardConfigCommon.mk
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(DEVICE_PATH)/sepolicy/private
+
 # Properties
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
